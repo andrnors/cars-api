51d90fb06baaad0ec211921fe43c0cb5109faaca
Andreas <andrnors@stud.ntnu.no>
2017-03-18 15:37:25 +0100

    scraping files

---
+++ scrape_wanted_cars.py
@@ -0,0 +1,128 @@
+import urllib.request
+from urllib.request import Request, urlopen
+import re
+from bs4 import BeautifulSoup
+import json
+
+
+def find(source, first, last):
+    try:
+        start = source.index(first) + len(first)
+        end = source.index(last, start)
+        return source[start:end]
+    except ValueError:
+        return ""
+
+def scrapeAllSivil():
+    req = Request('https://regnr.info/sivilpoliti', headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    soup = BeautifulSoup(webpage, 'html.parser')
+    cars = soup.find_all("div", class_="bilboks-container")
+
+    car_soup = BeautifulSoup(str(cars), 'html.parser')
+
+    alle_registreringsnummer = car_soup.find_all("b")
+    alle_merker = car_soup.find_all("div", class_="bilboks-merkemodell")
+    alle_bilder = car_soup.find_all("div", class_="bilboks-bilde")
+
+    alle_biler = []
+    for i in range(len(alle_registreringsnummer)):
+        regnr = alle_registreringsnummer[i].string
+        merke_model = alle_merker[i].string.strip()
+        bilde = str(alle_bilder[i])[37:-9]
+        data = {'regnr':regnr, 'merke_model':merke_model, 'bilde':bilde}
+        alle_biler.append(data)
+
+    with open('sivil.json', 'w') as outfile:
+        json.dump(alle_biler, outfile, indent=4)
+
+
+
+def scrapeSingleSivil(regnr):
+    req = Request('https://regnr.info/' + regnr, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    soup = BeautifulSoup(webpage, 'html.parser')
+
+    kjoretoy_info = str(soup.find_all("div", class_="kjoretoy-data"))
+    kategori = find(kjoretoy_info, "<b>", "</b><br/>").strip()
+    type_bil = find(kjoretoy_info, "<h1>","</h1>").strip()
+    tilhorer = find(kjoretoy_info, '<a href="/sivilpoliti" style="text-decoration:none;color:red;font-weight:bold;">', "</a>").strip()
+    info_politi = find(kjoretoy_info, "</a>\n<br/>", '</span></div>').strip()
+
+    serienummer = find(str(soup.find_all("li")), '<a href="/artikler/serienummer">Serienummeret</a> er','.</li>').strip()
+
+    # motor
+    motor_info = soup.find_all("table", class_="noheader")
+    # print(motor_info[1])
+    motor_info = str(motor_info[1])
+    slagvolum = find(motor_info, "</sup><br/>", '</td>').strip()
+    motorytelse = find(motor_info, '<td>Motorytelse</td>\n<td>','<br/>').strip()
+    aksler_med_drift = find(motor_info, '<td>Aksler med drift</td>\n<td>','</td>').strip()
+
+
+    kommentarer = soup.find_all("td")
+    alle_kommentarer = []
+    for kommentar in kommentarer:
+        if "inline-kommentar-dato" in str(kommentar):
+            kommentar = find(str(kommentar), "</b></span>", "</td>").strip()
+            alle_kommentarer.append(kommentar)
+
+    car_object = {  "kategori":kategori,
+                    "type_bil": type_bil,
+                    "tilhorer":tilhorer,
+                    "info_politi":info_politi,
+                    "slagvolum":slagvolum,
+                    "motorytelse":motorytelse,
+                    "aksler_med_drift":aksler_med_drift,
+                    "serienummer": serienummer,
+                    "kommentarer":alle_kommentarer
+                }
+    jsonarray = json.dumps(car_object, indent=4)
+    return jsonarray
+
+# scrapeSingleSivil("BP99615")
+
+
+def scrapeNormalCar(regnr):
+    req = Request('https://regnr.info/' + regnr, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    soup = BeautifulSoup(webpage, 'html.parser')
+
+    # Check if car is stolen
+    stolen = str(soup.find(id="warning-stolen"))
+    if(stolen != None): #car is stolen
+        stolen = find(str(stolen), "<h1>", "\n").strip()
+    else:
+        stolen = ""
+
+    # kommentarer = soup.find_all("span", class_="inline-kommentar-dato")
+    kjoretoy_info = str(soup.find_all("div", class_="kjoretoy-data"))
+    kategori = find(kjoretoy_info, "<b>", "</b><br/>").strip()
+    type_bil = find(kjoretoy_info, "<h1>","</h1>").strip()
+    info_normal_eier = find(kjoretoy_info, '<div class="kjoretoy-data">\n<span>','</span></div>').strip()
+    serienummer = find(str(soup.find_all("li")), '<a href="/artikler/serienummer">Serienummeret</a> er','.</li>').strip()
+
+    # motor
+    motor_info = soup.find_all("table", class_="noheader")
+    # print(motor_info[1])
+    motor_info = str(motor_info[1])
+    slagvolum = find(motor_info, "</sup><br/>", '</td>').strip()
+    motorytelse = find(motor_info, '<td>Motorytelse</td>\n<td>','<br/>').strip()
+    aksler_med_drift = find(motor_info, '<td>Aksler med drift</td>\n<td>','</td>').strip()
+
+    car_object = {
+        "stolen": stolen,
+        "kategori": kategori,
+        "type_bil": type_bil,
+        "info_normal_eier": info_normal_eier,
+        "serienummer": serienummer,
+        "slagvolum": slagvolum,
+        "motorytelse":motorytelse,
+        "aksler_med_drift":aksler_med_drift
+    }
+    print(car_object)
+    jsonarray = json.dumps(car_object, indent=4)
+    return jsonarray
+
+
+scrapeNormalCar("AU6724")
